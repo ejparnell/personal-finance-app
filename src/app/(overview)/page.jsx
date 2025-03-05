20d5ccd98a3b76019e9cead867169d38e45fb200
Elizabeth Parnell <elizabethprnll@gmail.com>
2025-03-05 10:19:25 -0500

    Desktop and tablet views for overview page complete.

--- src/app/(overview)/page.jsx
+++ src/app/(overview)/page.jsx
@@ -131,137 +131,141 @@ export default function OverviewPage() {
                 </div>
             </section>
 
-            {/* Pots Overview */}
-            <section className={`${styles.overview__card} ${styles.overview__pots}`}>
-                <div className={`${styles['overview__card-header']} ${styles['overview__pots-header']}`}>
-                    <h2 className={styles['overview__card-title']}>Pots</h2>
-                    <Link className={styles['overview__card-link']} href='/pots'>
-                        See Details
-                        <span className={styles['overview__card-arrow']}>
-                            <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
-                        </span>
-                    </Link>
-                </div>
-                <div className={styles['overview__pots-saved']}>
-                    <Image className={styles['overview__pots-image']} src='/assets/images/icon-pot.svg' alt='Pot' width={40} height={40} />
-                    <div>
-                        <p className={styles['overview__pots-text']}>Total Saved</p>
-                        <p className={styles['overview__pots-amount']}>{currencyFormatter(calculateTotalPotSaved(pots))}</p>
-                    </div>
-                </div>
-                <div className={styles['overview__card-list']}>
-                    {/* Only Showing 4 Pots */}
-                    {pots.length > 0 ? pots.slice(0, 4).map((pot) => (
-                        <div className={styles['overview__card-item']} key={pot._id}>
-                            <span style={{ backgroundColor: pot.theme }} className={styles['overview__card-indicator']}></span>
-                            <p className={styles['overview__card-text']}>{pot.name}</p>
-                            <p className={styles['overview__card-amount']}>{currencyFormatter(pot.total)}</p>
+            <div className={styles.overview__deatials}>
+                <div className={styles.overview__deatial}>
+                    {/* Pots Overview */}
+                    <section className={`${styles.overview__card} ${styles.overview__pots}`}>
+                        <div className={`${styles['overview__card-header']} ${styles['overview__pots-header']}`}>
+                            <h2 className={styles['overview__card-title']}>Pots</h2>
+                            <Link className={styles['overview__card-link']} href='/pots'>
+                                See Details
+                                <span className={styles['overview__card-arrow']}>
+                                    <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
+                                </span>
+                            </Link>
                         </div>
-                    )) : <p className={styles['overview__card-text']}>No pots</p>}
-                </div>
-            </section>
-
-            {/* Transactions Overview */}
-            <section className={`${styles.overview__card} ${styles.overview__transactions}`}>
-                <div className={`${styles['overview__card-header']} ${styles['overview__transactions-header']}`}>
-                    <h2 className={styles['overview__card-title']}>Transactions</h2>
-                    <Link className={styles['overview__card-link']} href='/transactions'>
-                        View All
-                        <span className={styles['overview__card-arrow']}>
-                            <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
-                        </span>
-                    </Link>
-                </div>
-                <div>
-                    {/* Only Showing 5 Transactions - Sorted */}
-                    {transactions.length > 0 ? topFiveTransactions.map((transaction, index) => (
-                        <div className={styles['overview__transactions-items']} key={transaction._id}>
-                            <div className={styles['overview__transactions-item']}>
-                                <div className={styles['overview__transactions-details']}>
-                                    <Image
-                                        className={styles['overview__transactions-image']}
-                                        src={transaction.avatar ? `/${transaction.avatar}` : '/assets/images/icon-payments.svg'}
-                                        alt={transaction.name}
-                                        width={32}
-                                        height={32}
-                                    />
-                                    <p className={styles['overview__transactions-name']}>{transaction.name}</p>
-                                </div>
-                                <div className={styles['overview__transactions-breakdown']}>
-                                    <p style={{ color: formatTransactionAmount(transaction.amount).color }} className={styles['overview__transactions-amount']}>
-                                        {formatTransactionAmount(transaction.amount).formattedValue}
-                                    </p>
-                                    <p className={styles['overview__transactions-date']}>{formatDate(transaction.date)}</p>
-                                </div>
+                        <div className={styles['overview__pots-saved']}>
+                            <Image className={styles['overview__pots-image']} src='/assets/images/icon-pot.svg' alt='Pot' width={40} height={40} />
+                            <div>
+                                <p className={styles['overview__pots-text']}>Total Saved</p>
+                                <p className={styles['overview__pots-amount']}>{currencyFormatter(calculateTotalPotSaved(pots))}</p>
                             </div>
-                            {index !== topFiveTransactions.length - 1 && <hr className={styles['overview__transactions-hr']} />}
                         </div>
-                    )) : <p className={styles['overview__card-text']}>No transactions</p>}
-                </div>
+                        <div className={styles['overview__card-list']}>
+                            {/* Only Showing 4 Pots */}
+                            {pots.length > 0 ? pots.slice(0, 4).map((pot) => (
+                                <div className={styles['overview__card-item']} key={pot._id}>
+                                    <span style={{ backgroundColor: pot.theme }} className={styles['overview__card-indicator']}></span>
+                                    <p className={styles['overview__card-text']}>{pot.name}</p>
+                                    <p className={styles['overview__card-amount']}>{currencyFormatter(pot.total)}</p>
+                                </div>
+                            )) : <p className={styles['overview__card-text']}>No pots</p>}
+                        </div>
+                    </section>
 
-            </section>
-
-            {/* Budgets Overview */}
-            <section className={`${styles.overview__card} ${styles.overview__budgets}`}>
-                <div className={styles['overview__card-header']}>
-                    <h2 className={styles['overview__card-title']}>Budgets</h2>
-                    <Link className={styles['overview__card-link']} href='/budgets'>
-                        See Details
-                        <span className={styles['overview__card-arrow']}>
-                            <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
-                        </span>
-                    </Link>
+                    {/* Transactions Overview */}
+                    <section className={`${styles.overview__card} ${styles.overview__transactions}`}>
+                        <div className={`${styles['overview__card-header']} ${styles['overview__transactions-header']}`}>
+                            <h2 className={styles['overview__card-title']}>Transactions</h2>
+                            <Link className={styles['overview__card-link']} href='/transactions'>
+                                View All
+                                <span className={styles['overview__card-arrow']}>
+                                    <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
+                                </span>
+                            </Link>
+                        </div>
+                        <div>
+                            {/* Only Showing 5 Transactions - Sorted */}
+                            {transactions.length > 0 ? topFiveTransactions.map((transaction, index) => (
+                                <div className={styles['overview__transactions-items']} key={transaction._id}>
+                                    <div className={styles['overview__transactions-item']}>
+                                        <div className={styles['overview__transactions-details']}>
+                                            <Image
+                                                className={styles['overview__transactions-image']}
+                                                src={transaction.avatar ? `/${transaction.avatar}` : '/assets/images/icon-payments.svg'}
+                                                alt={transaction.name}
+                                                width={32}
+                                                height={32}
+                                            />
+                                            <p className={styles['overview__transactions-name']}>{transaction.name}</p>
+                                        </div>
+                                        <div className={styles['overview__transactions-breakdown']}>
+                                            <p style={{ color: formatTransactionAmount(transaction.amount).color }} className={styles['overview__transactions-amount']}>
+                                                {formatTransactionAmount(transaction.amount).formattedValue}
+                                            </p>
+                                            <p className={styles['overview__transactions-date']}>{formatDate(transaction.date)}</p>
+                                        </div>
+                                    </div>
+                                    {index !== topFiveTransactions.length - 1 && <hr className={styles['overview__transactions-hr']} />}
+                                </div>
+                            )) : <p className={styles['overview__card-text']}>No transactions</p>}
+                        </div>
+                    </section>
                 </div>
-                <div className={styles['overview__budgets-chart']}>
-                    <DonutChart
-                        data={budgets}
-                        height={240}
-                        innerRadius={70}
-                        total={calculateTotalBudgetSpent(transactions)}
-                        limit={calculateTotalBudget(budgets)}
-                    />
-                    <div className={styles['overview__budgets-list']}>
-                        {/* Only Showing 4 Budgets */}
-                        {budgets.length > 0 ? budgets.slice(0, 4).map((budget) => (
-                            <div className={styles['overview__card-item']} key={budget._id}>
-                                <span style={{ backgroundColor: budget.theme }} className={styles['overview__card-indicator']}></span>
-                                <p className={styles['overview__card-text']}>{budget.category}</p>
-                                <p className={styles['overview__card-amount']}>{currencyFormatter(budget.maximum)}</p>
+                <div className={styles.overview__deatial}>
+                    {/* Budgets Overview */}
+                    <section className={`${styles.overview__card} ${styles.overview__budgets}`}>
+                        <div className={styles['overview__card-header']}>
+                            <h2 className={styles['overview__card-title']}>Budgets</h2>
+                            <Link className={styles['overview__card-link']} href='/budgets'>
+                                See Details
+                                <span className={styles['overview__card-arrow']}>
+                                    <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
+                                </span>
+                            </Link>
+                        </div>
+                        <div className={styles['overview__budgets-chart']}>
+                            <DonutChart
+                                data={budgets}
+                                height={240}
+                                innerRadius={70}
+                                total={calculateTotalBudgetSpent(transactions)}
+                                limit={calculateTotalBudget(budgets)}
+                            />
+                            <div className={styles['overview__budgets-list']}>
+                                {/* Only Showing 4 Budgets */}
+                                {budgets.length > 0 ? budgets.slice(0, 4).map((budget) => (
+                                    <div className={styles['overview__card-item']} key={budget._id}>
+                                        <span style={{ backgroundColor: budget.theme }} className={styles['overview__card-indicator']}></span>
+                                        <p className={styles['overview__card-text']}>{budget.category}</p>
+                                        <p className={styles['overview__card-amount']}>{currencyFormatter(budget.maximum)}</p>
+                                    </div>
+                                )) : <p className={styles['overview__card-text']}>No budgets</p>}
                             </div>
-                        )) : <p className={styles['overview__card-text']}>No budgets</p>}
-                    </div>
-                </div>
-            </section>
+                        </div>
+                    </section>
 
-            {/* Recurring Bills */}
-            <section className={`${styles.overview__card} ${styles.overview__bills}`}>
-                <div className={styles['overview__card-header']}>
-                    <h2 className={styles['overview__card-title']}>Recurring Bills</h2>
-                    <Link className={styles['overview__card-link']} href='/recurring-bills'>
-                        See Details
-                        <span className={styles['overview__card-arrow']}>
-                            <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
-                        </span>
-                    </Link>
+                    {/* Recurring Bills */}
+                    <section className={`${styles.overview__card} ${styles.overview__bills}`}>
+                        <div className={styles['overview__card-header']}>
+                            <h2 className={styles['overview__card-title']}>Recurring Bills</h2>
+                            <Link className={styles['overview__card-link']} href='/recurring-bills'>
+                                See Details
+                                <span className={styles['overview__card-arrow']}>
+                                    <Image src='/assets/images/icon-caret-right.svg' alt='Arrow Right' width={CARD_ARROW_SIZE} height={CARD_ARROW_SIZE} />
+                                </span>
+                            </Link>
+                        </div>
+                        <div className={styles['overview__bills-items']}>
+                            <div className={styles['overview__bills-item']}>
+                                <span style={{ backgroundColor: '#277C78' }} className={styles['overview__card-indicator']}></span>
+                                <p className={styles['overview__bills-text']}>Paid Bills</p>
+                                <p className={styles['overview__bills-amount']}>{currencyFormatter(calculatePaidBills(recurringBills))}</p>
+                            </div>
+                            <div className={styles['overview__bills-item']}>
+                                <span style={{ backgroundColor: '#F2CDAC' }} className={styles['overview__card-indicator']}></span>
+                                <p className={styles['overview__bills-text']}>Total Upcoming</p>
+                                <p className={styles['overview__bills-amount']}>{currencyFormatter(calculateTotalUpcoming(recurringBills))}</p>
+                            </div>
+                            <div className={styles['overview__bills-item']}>
+                                <span style={{ backgroundColor: '#82C9D7' }} className={styles['overview__card-indicator']}></span>
+                                <p className={styles['overview__bills-text']}>Due Soon</p>
+                                <p className={styles['overview__bills-amount']}>{currencyFormatter(calculateDueSoon(recurringBills))}</p>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-                <div>
-                    <div className={styles['overview__bills-item']}>
-                        <span style={{ backgroundColor: '#277C78' }} className={styles['overview__card-indicator']}></span>
-                        <p className={styles['overview__bills-text']}>Paid Bills</p>
-                        <p className={styles['overview__bills-amount']}>{currencyFormatter(calculatePaidBills(recurringBills))}</p>
-                    </div>
-                    <div className={styles['overview__bills-item']}>
-                        <span style={{ backgroundColor: '#F2CDAC' }} className={styles['overview__card-indicator']}></span>
-                        <p className={styles['overview__bills-text']}>Total Upcoming</p>
-                        <p className={styles['overview__bills-amount']}>{currencyFormatter(calculateTotalUpcoming(recurringBills))}</p>
-                    </div>
-                    <div className={styles['overview__bills-item']}>
-                        <span style={{ backgroundColor: '#82C9D7' }} className={styles['overview__card-indicator']}></span>
-                        <p className={styles['overview__bills-text']}>Due Soon</p>
-                        <p className={styles['overview__bills-amount']}>{currencyFormatter(calculateDueSoon(recurringBills))}</p>
-                    </div>
-                </div>
-            </section>
+            </div>
         </>
     )
 }
